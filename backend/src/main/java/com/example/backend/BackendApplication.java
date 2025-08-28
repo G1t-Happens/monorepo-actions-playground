6c8217122d78c1315e152d103a3a52862e209a36
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-08-28 17:31:48 +0200

    chore: update comment in backend (#33)

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Start here
+//Start
 @SpringBootApplication
 public class BackendApplication {
 
